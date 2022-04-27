9b870419a4c13f41a0a575b7e01e7f5587bfb2bc
Alexsandro Ryan C <alexsandrocosta855@gmail.com>
2022-04-27 19:50:35 -0300

    Criação do Novo App Bytebank

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:newbytebank/screens/dashboard.dart';
+
+import 'screens/contacts_list.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        appBarTheme: AppBarTheme(color: Colors.green[900]),
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: Colors.green[900]),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
+
+
+
